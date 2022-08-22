bce2317274416e8052343297761c259887eda800
BenjaGO1 <101335426+BenjaGO1@users.noreply.github.com>
2022-08-22 12:05:42 -0300

    Initial commit

---
+++ Assets/Scripts/ChangeSceneOnClick.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneOnClick : MonoBehaviour
+{
+
+    int puntaje = PlayerPrefs.GetInt("puntaje");
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log(gameObject.name);
+        PlayerPrefs.SetString("nombre", gameObject.name);
+        PlayerPrefs.SetInt("puntaje", puntaje + 1);
+        SceneManager.LoadScene("Escena final");
+    }
+}
---
+++ Assets/Scripts/SO/GameData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameData", menuName = "SO Gamedata")]
+public class GameData : ScriptableObject
+{
+    public int puntaje;
+}
